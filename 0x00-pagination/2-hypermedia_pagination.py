34d44f1abdcf9da9ecc12290026639c91d2e22eb
Natiman58 <natiman58@gmail.com>
2023-01-22 11:23:56 +0300

    get_hyper

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -43,3 +43,27 @@ class Server:
             return data[idx[0]:idx[1]]
         except IndexError:
             return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+            page_size: the length of the returned data set page
+            page: the current page number
+            data: the dataset page
+            next_page: number of the next page, None if no page
+            prev_page: number of the previous page, None if no page
+            total_pages: total number of pages in the data set
+            return a dict of the above info
+        """
+        total_pages = (len(self.dataset()) // page_size) + 1
+
+        data = self.get_page(page, page_size)
+
+        dict_t = {
+            'page_size': page_size if page_size <= len(data) else len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page + 1 <= total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
+        return dict_t
