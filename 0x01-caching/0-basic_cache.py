3bd4cea22f09983cb444563af7814598fc267dfc
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-01-24 12:05:32 +0300

    caching system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+    A base class module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+        A base class that inherit from base cache class
+    """
+    def __init__(self):
+        """
+            initialize this class using the parent class
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+            Assigns the item to the key
+            and add the item to the cached data dict
+            if no key or item; dont print anythiing
+        """
+        if key or item is None:
+            pass
+        self.cache_data[key] = item
+    
+    def get(self, key):
+        """
+            returns the value associated with the assigned key
+            from the cached data dict
+            if there is no key or key is not in the dict return None
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
