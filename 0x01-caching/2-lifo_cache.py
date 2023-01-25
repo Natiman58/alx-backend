5f9b261ad37be3cda1a0ab6f032904fddaa87c5a
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-01-25 13:51:26 +0300

    pycodestyle

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -43,4 +43,4 @@ class LIFOCache(BaseCaching):
         """
         if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
-        return  None
+        return None
