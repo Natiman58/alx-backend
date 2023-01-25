f3a4b004a3a18d6ea525a12527374a5527400255
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-01-25 11:59:43 +0300

    key

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -40,7 +40,7 @@ class LRUCache(BaseCaching):
         """
             returns the value of the item linked to the key
         """
-        if key is None or key not in self.cache_data:
+        if key is None and key not in self.cache_data:
             return None
         return self.cache_data[key]
 
