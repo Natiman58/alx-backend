f82e36b5eec475ee0aba5d7fbe0b4c4aeed047a7
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-01-31 12:57:48 +0300

    set the available languages

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This is a simple script to add flak-babel to
+the flask application
+"""
+
+from flask import Flask
+from flask_babel import Babel
+
+app = Flask(__name__)  # instantiate Flask object
+babel = Babel(app)  # instantiate Bable object
+
+
+class Config(object):
+    """
+    Flask configuration for available languages in our app
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'  # default language
+    BABEL_DEFAULT_TIMEZONE = 'UTC'  # default timezone
