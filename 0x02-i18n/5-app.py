99fc6826bdd8d0cd72f145ada3e6cc9d59089524
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-01-31 23:51:13 +0300

    rearrange

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -6,13 +6,6 @@
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
 
 class Config(object):
     """
@@ -27,6 +20,12 @@ class Config(object):
 app = Flask(__name__)  # instantiate Flask object
 app.config.from_object(Config)
 babel = Babel(app)  # instantiate Babel object
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 @babel.localeselector
