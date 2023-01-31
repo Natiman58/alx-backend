0cbd3e5d05afde8437b4c4f2b690532649fb69c0
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-01-31 22:30:32 +0300

    login mock

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -16,7 +16,7 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = "en"  # default language
     BABEL_DEFAULT_TIMEZONE = "UTC"  # default timezone
 
-
+# instantiate objs after configuration
 app = Flask(__name__)  # instantiate Flask object
 app.config.from_object(Config)
 babel = Babel(app)  # instantiate Babel object
@@ -28,7 +28,7 @@ def get_locale():
     Get the current locale
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-# for newer versions of Babel use this instade of @bable.localeselector
+# for newer versions of Babel(2.11.0) use this instade of @bable.localeselector
 # babel.init_app(app, locale_selector=get_locale)  # add locale_selector
 
 
--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -3,8 +3,15 @@
     To force a particular locale using a user's request
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config(object):
@@ -15,7 +22,8 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = "en"  # default language
     BABEL_DEFAULT_TIMEZONE = "UTC"  # default timezone
 
-# instantiate objs after configuration is complete
+
+# instantiate objects after configuration
 app = Flask(__name__)  # instantiate Flask object
 app.config.from_object(Config)
 babel = Babel(app)  # instantiate Babel object
@@ -40,13 +48,31 @@ def get_locale():
 # for newer versions of Babel(2.11.0) use this instade of @bable.localeselector
 # babel.init_app(app, locale_selector=get_locale)  # add locale_selector
 
+def get_user():
+    """
+    Get the current user
+    """
+    user_id = request.args.get('login_as', None)
+    if user_id is not None and int(user_id) in users.keys():
+        return users[int(user_id)]
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Before each request check if a user if any exist
+    and add it to the global flask.g
+    """
+    g.user = get_user()
+
 
 @app.route('/', strict_slashes=False)
 def index():
     """
     Homepage route
     """
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
