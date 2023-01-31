fdd1f02db38251cba39cfc959e84abc7864d31c7
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-01-31 12:21:38 +0300

    setup flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    A script to setup a flask application
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """
+    Renders the home page
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
