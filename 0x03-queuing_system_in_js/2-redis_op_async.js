40ade05c111f912bc69e2d8508583a9694dc4159
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-02-27 13:51:07 +0300

    implement the async version of the client.get method

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,37 @@
+import redis from 'redis';
+
+// import promisify
+const { promisify } = require('util')
+
+// create a Redis client
+const client = redis.createClient();
+
+// get the promisified(async) version of client.get function
+const getAsync = promisify(client.get).bind(client)
+
+// when client is connected to Redis
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+// when there is error on connecting to Redis
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+});
+
+// add new function
+function setNewSchool(schoolName, value) {
+    client.SET(schoolName, value, redis.print);
+}
+
+// add another function displaySchoolValue
+async function displaySchoolValue(schoolName) {
+    const value = await getAsync(schoolName);
+    console.log(value)
+}
+
+
+displaySchoolValue('Holberton');
+
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
